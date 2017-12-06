cc3446fc6053f4d058b397908dab677ac5f4e511
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-06 10:40:57 +0900

    update devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -12,69 +12,67 @@ const isPlainObj = require('is-plain-obj');
 const PATH_ERROR = 'Expected a file or directory path to get its mode';
 
 const confusingOptionNames = new Set([
-  'followSymlink',
-  'resolveSymlink',
-  'resolveSymlinks'
+	'followSymlink',
+	'resolveSymlink',
+	'resolveSymlinks'
 ]);
 
 module.exports = function getMode(...args) {
-  return new Promise((resolve, reject) => {
-    const argLen = args.length;
+	return new Promise((resolve, reject) => {
+		const argLen = args.length;
 
-    if (argLen !== 1 && argLen !== 2) {
-      throw new TypeError(`Expected 1 or 2 arguments (path: String[, option: Object]), but got ${
-        argLen === 0 ? 'no' : argLen
-      } arguments.`);
-    }
+		if (argLen !== 1 && argLen !== 2) {
+			throw new TypeError(`Expected 1 or 2 arguments (path: String[, option: Object]), but got ${
+				argLen === 0 ? 'no' : argLen
+			} arguments.`);
+		}
 
-    const [path, option] = args;
+		const [path, option] = args;
 
-    if (typeof path !== 'string') {
-      throw new TypeError(`${PATH_ERROR}, but got a non-string value ${inspectWithKind(path)}.`);
-    }
+		if (typeof path !== 'string') {
+			throw new TypeError(`${PATH_ERROR}, but got a non-string value ${inspectWithKind(path)}.`);
+		}
 
-    if (path.length === 0) {
-      throw new Error(`${PATH_ERROR}, but got '' (empty string).`);
-    }
+		if (path.length === 0) {
+			throw new Error(`${PATH_ERROR}, but got '' (empty string).`);
+		}
 
-    function cb(err, result) {
-      if (err) {
-        reject(err);
-        return;
-      }
+		function cb(err, result) {
+			if (err) {
+				reject(err);
+				return;
+			}
 
-      resolve(result.mode);
-    }
+			resolve(result.mode);
+		}
 
-    if (argLen === 1) {
-      lstat(path, cb);
-      return;
-    }
+		if (argLen === 1) {
+			lstat(path, cb);
+			return;
+		}
 
-    if (!isPlainObj(option)) {
-      throw new TypeError(
-        `Expected an object to specify whether \`followSymlinks\` option is enabled or not, but got ${
-          inspectWithKind(option)
-        }.`
-      );
-    }
+		if (!isPlainObj(option)) {
+			throw new TypeError(`Expected an object to specify whether \`followSymlinks\` option is enabled or not, but got ${
+				inspectWithKind(option)
+			}.`);
+		}
 
-    for (const optionName of confusingOptionNames) {
-      const val = option[optionName];
+		for (const optionName of confusingOptionNames) {
+			const val = option[optionName];
 
-      if (val !== undefined) {
-        throw new Error(`\`${optionName}\` option is not supported but ${
-          inspectWithKind(val)
-        } was provided for it. You mistook \`followSymlinks\` as it.`);
-      }
-    }
+			if (val !== undefined) {
+				throw new Error(`\`${optionName}\` option is not supported but ${
+					inspectWithKind(val)
+				} was provided for it. You mistook \`followSymlinks\` as it.`);
+			}
+		}
 
-    if (option.followSymlinks && typeof option.followSymlinks !== 'boolean') {
-      throw new TypeError(`Expected \`followSymlinks\` option to be a Boolean value, but got ${
-        inspectWithKind(option.followSymlinks)
-      }.`);
-    }
+		if (option.followSymlinks && typeof option.followSymlinks !== 'boolean') {
+			throw new TypeError(`Expected \`followSymlinks\` option to be a Boolean value, but got ${
+				inspectWithKind(option.followSymlinks)
+			}.`);
+		}
 
-    (option.followSymlinks ? stat : lstat)(path, cb);
-  });
+		(option.followSymlinks ? stat : lstat)(path, cb);
+	});
 };
--- test.js
+++ test.js
@@ -6,123 +6,107 @@ const runParallel = require('run-parallel');
 const {test} = require('tap');
 
 function runStatAndLstat(path, cb) {
-  runParallel([
-    done => stat(path, (err, {mode}) => done(err, mode)),
-    done => lstat(path, (err, {mode}) => done(err, mode))
-  ], cb);
+	runParallel([
+		done => stat(path, (err, {mode}) => done(err, mode)),
+		done => lstat(path, (err, {mode}) => done(err, mode))
+	], cb);
 }
 
 test('get a file mode', t => {
-  t.plan(5);
+	t.plan(5);
 
-  symlink('index.js', 'tmp', symlinkErr => {
-    t.equal(symlinkErr, null);
+	symlink('index.js', 'tmp', symlinkErr => {
+		t.equal(symlinkErr, null);
 
-    getMode('tmp').then(mode => {
-      runStatAndLstat('tmp', (err, [statMode, lstatMode]) => {
-        t.equal(err, null);
-        t.notEqual(statMode, mode);
-        t.equal(lstatMode, mode);
+		getMode('tmp').then(mode => {
+			runStatAndLstat('tmp', (err, [statMode, lstatMode]) => {
+				t.equal(err, null);
+				t.notEqual(statMode, mode);
+				t.equal(lstatMode, mode);
 
-        unlink('tmp', unlinkErr => t.is(unlinkErr, null));
-      });
-    }, t.error);
-  });
+				unlink('tmp', unlinkErr => t.is(unlinkErr, null));
+			});
+		}, t.error);
+	});
 });
 
 test('`followSymlinks` option', t => {
-  t.plan(5);
+	t.plan(5);
 
-  symlink('index.js', '__tmp__', symlinkErr => {
-    t.equal(symlinkErr, null);
+	symlink('index.js', '__tmp__', symlinkErr => {
+		t.equal(symlinkErr, null);
 
-    getMode('__tmp__', {followSymlinks: true}).then(mode => {
-      runStatAndLstat('__tmp__', (err, [statMode, lstatMode]) => {
-        t.equal(err, null);
-        t.equal(statMode, mode);
-        t.notEqual(lstatMode, mode);
+		getMode('__tmp__', {followSymlinks: true}).then(mode => {
+			runStatAndLstat('__tmp__', (err, [statMode, lstatMode]) => {
+				t.equal(err, null);
+				t.equal(statMode, mode);
+				t.notEqual(lstatMode, mode);
 
-        unlink('__tmp__', unlinkErr => t.is(unlinkErr, null));
-      });
-    }, t.error);
-  });
+				unlink('__tmp__', unlinkErr => t.is(unlinkErr, null));
+			});
+		}, t.error);
+	});
 });
 
-test('be rejected on `lstat` failure', t => {
-  return getMode('not/found').catch(({code, message, name}) => {
-    t.equal(name, 'Error');
-    t.equal(code, 'ENOENT');
-    t.equal(message, 'ENOENT: no such file or directory, lstat \'not/found\'');
-  });
-});
+test('be rejected on `lstat` failure', t => getMode('not/found').catch(({code, message, name}) => {
+	t.equal(name, 'Error');
+	t.equal(code, 'ENOENT');
+	t.equal(message, 'ENOENT: no such file or directory, lstat \'not/found\'');
+}));
 
-test('invalidate non-string path', t => {
-  return getMode([0]).catch(({message, name}) => {
-    t.equal(name, 'TypeError');
-    t.equal(
-      message,
-      'Expected a file or directory path to get its mode, but got a non-string value [ 0 ] (array).'
-    );
-  });
-});
+test('invalidate non-string path', t => getMode([0]).catch(({message, name}) => {
+	t.equal(name, 'TypeError');
+	t.equal(
+		message,
+		'Expected a file or directory path to get its mode, but got a non-string value [ 0 ] (array).'
+	);
+}));
 
-test('invalidate empty path', t => {
-  return getMode('').catch(({message, name}) => {
-    t.equal(name, 'Error');
-    t.equal(
-      message,
-      'Expected a file or directory path to get its mode, but got \'\' (empty string).'
-    );
-  });
-});
+test('invalidate empty path', t => getMode('').catch(({message, name}) => {
+	t.equal(name, 'Error');
+	t.equal(
+		message,
+		'Expected a file or directory path to get its mode, but got \'\' (empty string).'
+	);
+}));
 
-test('invalidate non-object option', t => {
-  return getMode(__filename, Buffer.from('Hi')).catch(({message, name}) => {
-    t.equal(name, 'TypeError');
-    t.match(
-      message,
-      'whether `followSymlinks` option is enabled or not, but got <Buffer 48 69>.'
-    );
-  });
-});
+test('invalidate non-object option', t => getMode(__filename, Buffer.from('Hi')).catch(({message, name}) => {
+	t.equal(name, 'TypeError');
+	t.match(
+		message,
+		'whether `followSymlinks` option is enabled or not, but got <Buffer 48 69>.'
+	);
+}));
 
-test('invalidate non-boolean `followSymlinks` option', t => {
-  return getMode(__filename, {followSymlinks: new Uint8Array()}).catch(({message, name}) => {
-    t.equal(name, 'TypeError');
-    t.equal(
-      message,
-      'Expected `followSymlinks` option to be a Boolean value, but got Uint8Array [  ].'
-    );
-  });
-});
+test('invalidate non-boolean `followSymlinks` option', t => getMode(__filename, {followSymlinks: new Uint8Array()}).catch(({message, name}) => {
+	t.equal(name, 'TypeError');
+	t.equal(
+		message,
+		'Expected `followSymlinks` option to be a Boolean value, but got Uint8Array [  ].'
+	);
+}));
 
-test('invalidate unsupported option', t => {
-  return getMode(__filename, {followSymlink: Promise.resolve(1)}).catch(({message, name}) => {
-    t.equal(name, 'Error');
-    t.equal(
-      message,
-      '`followSymlink` option is not supported but Promise { 1 } was provided for it. ' +
+test('invalidate unsupported option', t => getMode(__filename, {followSymlink: -0}).catch(({message, name}) => {
+	t.equal(name, 'Error');
+	t.equal(
+		message,
+		'`followSymlink` option is not supported but -0 (number) was provided for it. ' +
       'You mistook `followSymlinks` as it.'
-    );
-  });
-});
+	);
+}));
 
-test('no arguments', t => {
-  return getMode().catch(({message, name}) => {
-    t.equal(name, 'TypeError');
-    t.equal(
-      message,
-      'Expected 1 or 2 arguments (path: String[, option: Object]), but got no arguments.'
-    );
-  });
-});
+test('no arguments', t => getMode().catch(({message, name}) => {
+	t.equal(name, 'TypeError');
+	t.equal(
+		message,
+		'Expected 1 or 2 arguments (path: String[, option: Object]), but got no arguments.'
+	);
+}));
 
-test('toomany arguments', t => {
-  return getMode('a', 'b', 'c').catch(({message, name}) => {
-    t.equal(name, 'TypeError');
-    t.equal(
-      message,
-      'Expected 1 or 2 arguments (path: String[, option: Object]), but got 3 arguments.'
-    );
-  });
-});
+test('toomany arguments', t => getMode('a', 'b', 'c').catch(({message, name}) => {
+	t.equal(name, 'TypeError');
+	t.equal(
+		message,
+		'Expected 1 or 2 arguments (path: String[, option: Object]), but got 3 arguments.'
+	);
+}));
