458d7b1fbf156763a92720f0a3e8346307180036
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-04-30 10:40:58 +0900

    use util.promisify() to simplify the code
    [BREAKING] drop support for Node.js < 8

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-const {lstat, stat} = require('fs');
+const {lstat: lstatCallback, stat: statCallback} = require('fs');
+const {inspect, promisify} = require('util');
 
 const inspectWithKind = require('inspect-with-kind');
 const isPlainObj = require('is-plain-obj');
 
 const EMPTY_PATH_ERROR = 'Expected a file or directory path to get its mode';
+const lstat = promisify(lstatCallback);
+const stat = promisify(statCallback);
 
 const confusingOptionNames = new Set([
 	'followSymlink',
@@ -14,61 +17,49 @@ const confusingOptionNames = new Set([
 ]);
 
 module.exports = async function getMode(...args) {
-	return new Promise((resolve, reject) => {
-		const argLen = args.length;
+	const argLen = args.length;
 
-		if (argLen !== 1 && argLen !== 2) {
-			throw new TypeError(`Expected 1 or 2 arguments (path: <string|Buffer|URL>[, option: <Object>]), but got ${
-				argLen === 0 ? 'no' : argLen
-			} arguments.`);
+	if (argLen !== 1 && argLen !== 2) {
+		throw new TypeError(`Expected 1 or 2 arguments (path: <string|Buffer|URL>[, option: <Object>]), but got ${
+			argLen === 0 ? 'no' : argLen
+		} arguments.`);
+	}
+
+	const [path, option] = args;
+
+	if (path === '') {
+		throw new Error(`${EMPTY_PATH_ERROR}, but got '' (empty string).`);
+	}
+
+	if (Buffer.isBuffer(path) && path.length === 0) {
+		throw new Error(`${EMPTY_PATH_ERROR}, but got an empty Buffer.`);
+	}
+
+	if (argLen === 1) {
+		return (await lstat(path)).mode;
+	}
+
+	if (!isPlainObj(option)) {
+		throw new TypeError(`Expected an object to specify whether \`followSymlinks\` option is enabled or not, but got ${
+			inspectWithKind(option)
+		}.`);
+	}
+
+	for (const optionName of confusingOptionNames) {
+		const val = option[optionName];
+
+		if (val !== undefined) {
+			throw new Error(`\`${optionName}\` option is not supported but a value ${
+				inspect(val)
+			} was provided for it. You mistook \`followSymlinks\` as it.`);
 		}
+	}
 
-		const [path, option] = args;
+	if (option.followSymlinks && typeof option.followSymlinks !== 'boolean') {
+		throw new TypeError(`Expected \`followSymlinks\` option to be a Boolean value, but got ${
+			inspectWithKind(option.followSymlinks)
+		}.`);
+	}
 
-		if (path === '') {
-			throw new Error(`${EMPTY_PATH_ERROR}, but got '' (empty string).`);
-		}
-
-		if (Buffer.isBuffer(path) && path.length === 0) {
-			throw new Error(`${EMPTY_PATH_ERROR}, but got an empty Buffer.`);
-		}
-
-		function cb(err, result) {
-			if (err) {
-				reject(err);
-				return;
-			}
-
-			resolve(result.mode);
-		}
-
-		if (argLen === 1) {
-			lstat(path, cb);
-			return;
-		}
-
-		if (!isPlainObj(option)) {
-			throw new TypeError(`Expected an object to specify whether \`followSymlinks\` option is enabled or not, but got ${
-				inspectWithKind(option)
-			}.`);
-		}
-
-		for (const optionName of confusingOptionNames) {
-			const val = option[optionName];
-
-			if (val !== undefined) {
-				throw new Error(`\`${optionName}\` option is not supported but ${
-					inspectWithKind(val)
-				} was provided for it. You mistook \`followSymlinks\` as it.`);
-			}
-		}
-
-		if (option.followSymlinks && typeof option.followSymlinks !== 'boolean') {
-			throw new TypeError(`Expected \`followSymlinks\` option to be a Boolean value, but got ${
-				inspectWithKind(option.followSymlinks)
-			}.`);
-		}
-
-		(option.followSymlinks ? stat : lstat)(path, cb);
-	});
+	return (await (option.followSymlinks ? stat : lstat)(path)).mode;
 };
